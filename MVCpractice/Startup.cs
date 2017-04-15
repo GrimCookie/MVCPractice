47f1d2ff11bb5a616e5be33d157f7fd0ea0650ff
steven mcvey <steven mcvey>
2017-04-15 13:02:51 +0100

    init commit

---
+++ MVCpractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCpractice.Startup))]
+namespace MVCpractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
